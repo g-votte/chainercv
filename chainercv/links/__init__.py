488851700b8914d63dccdd927a2923f199c575b3
Shotaro Sano <sano@preferred.jp>
2018-06-20 00:04:39 +0900

    Add SE-ResNet.

--- chainercv/links/__init__.py
+++ chainercv/links/__init__.py
@@ -9,6 +9,9 @@ from chainercv.links.model.faster_rcnn.faster_rcnn_vgg import FasterRCNNVGG16  #
 from chainercv.links.model.resnet import ResNet101  # NOQA
 from chainercv.links.model.resnet import ResNet152  # NOQA
 from chainercv.links.model.resnet import ResNet50  # NOQA
+from chainercv.links.model.senet import SEResNet101  # NOQA
+from chainercv.links.model.senet import SEResNet152  # NOQA
+from chainercv.links.model.senet import SEResNet50  # NOQA
 from chainercv.links.model.segnet.segnet_basic import SegNetBasic  # NOQA
 from chainercv.links.model.ssd import SSD300  # NOQA
 from chainercv.links.model.ssd import SSD512  # NOQA
